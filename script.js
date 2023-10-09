66d8851d6de716e926a90a3bfee8813d94253ced
Anıl Tarar <anil_tarar@hotmail.com>
2023-10-09 16:54:25 +0300

    Local Storage Seat Indexes settings are made it!

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ container.addEventListener("click",clickFunc)
 const ticketInfoSeatCount = document.querySelector("#ticketCount"); //Bilet Sayısı
 const ticketInfoMovieName = document.querySelector("#movie") //Seçilen Film
 const screenMovieName = document.querySelector("#screenMovieName"); //Perde'ye yansıtılan film
+const confirmButton = document.querySelector("#confirmButton");
+const seats = document.querySelectorAll(".container .seat");
+
 
 
 function clickFunc(e){
@@ -11,7 +14,8 @@ function clickFunc(e){
     e.target.classList.toggle('selected')
 
     // Seçilen Koltuk Sayısını Belirleme.
-    let selectedSeatCounter = container.querySelectorAll('.seat.selected').length;
+    let selectedSeats = container.querySelectorAll('.seat.selected');
+    let selectedSeatCounter = selectedSeats.length;
     ticketInfoSeatCount.innerHTML=selectedSeatCounter;
 
     //Seçilen Film Adını Görüntüleme.
@@ -22,5 +26,26 @@ function clickFunc(e){
     //Seçilen Film Bilet Fiyatı* Seçilen Koltuk Adedi
     let selectedMovieTicketPrice = document.querySelector("#price"); //info Text seçimi
     selectedMovieTicketPrice.innerText = ticketInfoMovieName.value*selectedSeatCounter;
-    
 }
+
+confirmButton.addEventListener("click",ticketReserved);
+
+
+function ticketReserved () {
+    let selectedSeats = container.querySelectorAll(".seat.selected");
+    let reservedTicketArr = [];
+
+    seats.forEach((seat, indexNum) => {
+        if(seat.classList.contains("selected")){
+            reservedTicketArr.push(indexNum)
+        }
+    });
+
+    selectedSeats.forEach(element => {
+        element.classList.remove("selected");
+        element.classList.add("reserved");
+    });
+
+    localStorage.setItem("Reserved-Seats",JSON.stringify(reservedTicketArr))
+}
+
